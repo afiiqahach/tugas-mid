33365494c73c942f095dbb6243ab386048c15ac3
afiqahdwi1@gmail.com <afiqahdwi1@gmail.com>
2023-10-29 23:31:47 +0800

    Commit kedua

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,30 @@
+// toggle class active menu
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
+
+// togle class active search
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+const sb = document.querySelector('#search-button');
+
+document.addEventListener('click', function(e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active')
+  }
+})
+
 
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
